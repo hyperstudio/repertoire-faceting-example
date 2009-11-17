7cc20ec7ebebfefefec3c9a43f1f5cd333867ec1
Christopher York <yorkc@Christopher-Yorks-MacBook.local>
2009-11-17 13:03:55 +0000

    Removed unnecessary files

--- bin/prettify_json.rb
+++
@@ -1,3 +0,0 @@
-#!/System/Library/Frameworks/Ruby.framework/Versions/1.8/usr/bin/ruby
-require File.join(File.dirname(__FILE__), "../gems/environment")
-load File.join(File.dirname(__FILE__), "../gems/gems/json-1.1.9/bin/prettify_json.rb")
